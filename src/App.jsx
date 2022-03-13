93f2d7adb6a1b501cb6c3f9594bc038e41a83b32
Abbracx <tankoraphael@gmail.com>
2022-03-13 12:20:05 +0100

    Initial setup with tailwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn">click</button>
+    </>
+  );
+}
+
+export default App;
