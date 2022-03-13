57766e4302bb20ce0d89de9cce3034680be06b6a
Abbracx <tankoraphael@gmail.com>
2022-03-13 19:01:12 +0100

    added pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/layouts/navbar";
-import Footer from "./components/layouts/footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound  from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        {/* <main className="container mx-auto px-3 pb-12"></main> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
